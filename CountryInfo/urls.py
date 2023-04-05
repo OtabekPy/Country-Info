8640f0316c80d06e79afea3706c84519097b9f1f
Admin <tursunovotabekkuva@gmail.com>
2023-04-05 09:16:27 +0500

    Swagger linki tahrirlandi

--- CountryInfo/urls.py
+++ CountryInfo/urls.py
@@ -32,6 +32,6 @@ urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('api/', include(router.urls)),
                   path('swagger<str:format>', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-                  path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+                  path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
                   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
